b9f831352f14fa8a135be86578e526517102c779
SebastianBuftea <sebibuftea85@gmail.com>
2024-05-28 16:10:58 +0200

    UPDATE WITH TEST

--- src/main/java/org/exercise/Calculator.java
+++ src/main/java/org/exercise/Calculator.java
@@ -1,2 +1,20 @@
-package org.exercise;public class Calculator {
+package org.exercise;
+
+public class Calculator {
+
+    public static float add(float num1, float num2){
+        return num1+num2;
+    }
+    public float substract(float num1, float num2){
+        return num1-num2;
+    }
+    public float divide(float num1, float num2){
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }        return num1/num2;
+    }
+    public float multiply(float num1, float num2){
+        return num1*num2;
+    }
 }
+
--- src/test/java/org/exercise/CalculatorTest.java
+++ src/test/java/org/exercise/CalculatorTest.java
@@ -1,4 +1,47 @@
+package org.exercise;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testadd() {
+        assertEquals(3.44F, calculator.add(1.22F, 2.22F));
+        assertEquals(0, calculator.add(-1, 1));
+        assertEquals(-2, calculator.add(-1, -1));
+    }
+
+    @Test
+    void testsubstract() {
+        assertEquals(5, calculator.substract(10.1F, 5.1F),0.1F);
+        assertEquals(-2, calculator.substract(-1, 1));
+        assertEquals(0, calculator.substract(-1, -1));
+    }
+
+    @Test
+    void testdivide() {
+        assertEquals(5, calculator.divide(10, 2));
+        assertEquals(-1, calculator.divide(-1, 1));
+        assertEquals(1, calculator.divide(-1, -1));
+        assertEquals(5.25, calculator.divide(10.5F, 2F));
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(10, 0));
+    }
+
+    @Test
+    void testmultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+        assertEquals(-1, calculator.multiply(-1, 1));
+        assertEquals(1, calculator.multiply(-1, -1));
+        assertEquals(57.75, calculator.multiply(10.5F, 5.5F));
+    }
 }
